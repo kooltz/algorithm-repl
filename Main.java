21bfa818046adce733933a710d7d1495ce78d60d
kooltz <hgshin83@gmail.com>
2020-05-07 23:23:51 +0900

    add comment

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("hello world");
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    StringBuilder sb = new StringBuilder();
+
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int r = Integer.parseInt(st.nextToken());
+      String s = st.nextToken();
+      for (int j = 0; j < s.length(); j++) {
+        for (int l = 0; l < r; l++) {
+          sb.append(s.charAt(j));
+        }
+      }
+      sb.append("\n");
+    }
+    
+    System.out.println(sb);
+    br.close();
   }
 }
